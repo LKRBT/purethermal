73d41e6dc471fd9be01c02af3c5e277ca8cdd3f9
KETI_UnstructedWork <tjdgus6255@naver.com>
2024-08-20 17:38:11 +0900

    use usb board on lepton

--- utils/camerahandler.py
+++ utils/camerahandler.py
@@ -1,37 +1,47 @@
-import sys
 import cv2
 import time
-import pygame
 import numpy as np
 
 from picamera2 import Picamera2
 from .thermalcamera import ThermalCamera
 
+from .send_sms import send_sms
+from .parallel import thread_method
 
-pygame.init()
-infoObject = pygame.display.Info()
-size_w = infoObject.current_w
-size_h = infoObject.current_h
-screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h), pygame.FULLSCREEN)
-
-clock = pygame.time.Clock()
-clock.tick(60)
 
 class CameraHandler:
     def __init__(self, cfg):
         self.cfg = cfg
+        
+        self.s_time = None
+        
+        self.ir_frame = None
+        self.rgb_frame = None
+        
+        self.sms_flag = False
+        self.fire_flag = False
+        
         self.picam2 = Picamera2()
         self.ircam = ThermalCamera()
         
-        self.ir_res = (160, 120)
-    
     def check_for_fire(self, data):
         temper = (data - 27315) / 100.0
         temper = temper.reshape(120, 160)
         _, max_val, _, max_loc = cv2.minMaxLoc(temper)
-
+        if max_val >= 60 and max_val < 100:
+            if self.s_time is None:
+                self.s_time = time.time()
+            elif time.time() - self.s_time >= 3:
+                self.fire_flag = True
+                if self.sms_flag:
+                    send_sms()
+                    self.sms_flag = False
+        else:
+            self.s_time = None
+            self.fire_flag = False
+            
         return "{:.2f}C".format(max_val), max_loc
-	
+    
     def capture(self):
         ir = self.ircam.capture()
         rgb = self.picam2.capture_array("main")
@@ -41,67 +51,34 @@ class CameraHandler:
     def process_ir(self, frame):
         frame = cv2.flip(frame, -1)
         
-        val, loc = self.check_for_fire(frame)
+        val, (x, y) = self.check_for_fire(frame)
         
         cv2.normalize(frame, frame, 0, 65535, cv2.NORM_MINMAX)
         np.right_shift(frame, 8, frame)
         frame = np.uint8(frame)
         frame = cv2.applyColorMap(frame, cv2.COLORMAP_PLASMA)
-        frame = cv2.resize(frame, (int(size_w/2), size_h), interpolation=cv2.INTER_NEAREST)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        scale_w = int(loc[0] * size_w / 2 / self.ir_res[0])
-        scale_h = int(loc[1] * size_h / self.ir_res[1])
-
-        cv2.line(frame, (scale_w - 20, scale_h), (scale_w - 5, scale_h), (255, 255, 255), 1)
-        cv2.line(frame, (scale_w + 5, scale_h), (scale_w + 20, scale_h), (255, 255, 255), 1)
-        cv2.line(frame, (scale_w, scale_h - 20), (scale_w, scale_h - 5), (255, 255, 255), 1)
-        cv2.line(frame, (scale_w, scale_h + 5), (scale_w, scale_h + 20), (255, 255, 255), 1)
-        cv2.circle(frame, (scale_w, scale_h), 5, (255, 255, 255), 1)
-        cv2.putText(frame, val, (scale_w + 20, scale_h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-
+        cv2.line(frame, (x - 10, y), (x - 2, y), (255, 255, 255), 1)
+        cv2.line(frame, (x + 2, y), (x + 10, y), (255, 255, 255), 1)
+        cv2.line(frame, (x, y - 10), (x, y - 2), (255, 255, 255), 1)
+        cv2.line(frame, (x, y + 2), (x, y + 10), (255, 255, 255), 1)
+        cv2.circle(frame, (x, y), 2, (255, 255, 255), 1)
+        cv2.putText(frame, val, (x + 10, y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (255, 255, 255), 1)
+        
         return frame
-	
+        
     def process_rgb(self, frame):
-        frame = cv2.resize(frame, (int(size_w/2), size_h), interpolation=cv2.INTER_NEAREST)
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2RGB)
-
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
         return frame
-
+        
+    @thread_method
     def loop(self):
-        running = True
         self.picam2.start()
-        while running:
+        while True:
             rgb, ir = self.capture()
-            view_ir = self.process_ir(ir)
-            view_rgb = self.process_rgb(rgb)
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == ord('q'):
-                        running = False
-                    if event.key == ord('c'):
-                        save_rgb = cv2.cvtColor(view_rgb, cv2.COLOR_RGB2BGR)
-                        save_ir = cv2.cvtColor(view_ir, cv2.COLOR_RGB2BGR)
-                        
-                        cv2.imwrite(self.cfg['PI']['FILE_PATH'] + 'ir_' + str(time.time()) + '.png', save_ir)
-                        cv2.imwrite(self.cfg['PI']['FILE_PATH'] + 'rgb_' + str(time.time()) + '.png', save_rgb)
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    print(event.button)
-                    if event.button == 3:
-                        running = False
-
-            rgb = pygame.surfarray.make_surface(view_rgb.swapaxes(0, 1))
-            ir = pygame.surfarray.make_surface(view_ir.swapaxes(0, 1))
-	
-            screen.fill((0, 0, 0))
-            screen.blit(rgb, (0, 0))
-            screen.blit(ir, (size_w/2, 0))
-            pygame.display.flip()
-	
-            clock.tick(60)
-            
-        pygame.quit()
-        sys.exit()
+            self.ir_frame = self.process_ir(ir)
+            self.rgb_frame = self.process_rgb(rgb)
+            if self.fire_flag:
+                cv2.imwrite(self.cfg['PI']['FILE_PATH'] + 'rgb_img.png', self.rgb_frame)
+                cv2.imwrite(self.cfg['PI']['FILE_PATH'] + 'ir_img.png', self.ir_frame)
